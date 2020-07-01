992297ac16365b15067dae67390065423be1cb6d
LeeEugene1 <uwm1004@gmail.com>
2020-07-01 22:38:35 +0900

    gugudan and plus wordrelay

--- gugudan/index.js
+++ gugudan/index.js
@@ -1,15 +1,15 @@
-document.querySelector('#click').addEventListener('click',()=>{
+document.querySelector('#click').addEventListener('click', () => {
     const f = document.querySelector('#first').value
     const s = document.querySelector('#second').value
-    
-    if(f){
-        if(s){
-            document.querySelector('#result').textContent = f*s
-        }else{
-            document.write('2번째값을 입력하세요')
+    const r = document.querySelector('#result').textContent
+    if (f) {
+        if (s) {
+            r.textContent = f * s
+        } else {
+            r.textContent = '2번째값을 입력하세요'
         }
-    }else{
-        document.write('1번째 값을 입력하세요')
+    } else {
+        r.textContent = '1번째 값을 입력하세요'
     }
 
 });
